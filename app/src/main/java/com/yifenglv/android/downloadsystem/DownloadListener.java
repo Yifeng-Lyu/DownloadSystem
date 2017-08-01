9de3592389cacd7c2c4e9303910bbaf453b042e0
yifenglv <yifenglv6@gmail.com>
2017-08-01 11:49:44 -0300

    Initial commit

---
+++ app/src/main/java/com/yifenglv/android/downloadsystem/DownloadListener.java
@@ -0,0 +1,16 @@
+package com.yifenglv.android.downloadsystem;
+
+
+public interface DownloadListener {
+
+    void onProgress(int progress);
+
+    void onSuccess();
+
+    void onFailed();
+
+    void onPaused();
+
+    void onCanceled();
+
+}
